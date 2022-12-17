306548e627abeb9eb6a1ca526dccd34775b0b4ba
andrewschwabe <schwabea530@gmail.com>
2022-12-17 12:55:48 -0600

    Add back button to answers page

--- Shared/AnswerModel.cs
+++ Shared/AnswerModel.cs
@@ -2,9 +2,11 @@
 {
     public class AnswerModel
     {
+
         public long Id { get; set; }
         public bool IsAccepted { get; set; }
         public string Body { get; set; }
+
 
         public string Class { get; set; } = string.Empty;
         public bool ButtonDisabled { get; set; } = false;
