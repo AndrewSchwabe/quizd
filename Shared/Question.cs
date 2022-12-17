6d3b264a348c5ed5c9c1366db899ae559fca937d
andrewschwabe <schwabea530@gmail.com>
2022-12-16 20:47:49 -0600

    Shell of questions component

---
+++ Shared/Question.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp.Shared
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int AnswerCount { get; set; }
+    }
+}
